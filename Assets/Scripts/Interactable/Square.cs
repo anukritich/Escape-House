7c584d92b2b56613c50769bcf93bc6a88ef8a8ba
anukritich <chyanukriti@gmail.com>
2024-04-17 16:48:37 +0530

    Interaction

---
+++ Assets/Scripts/Interactable/Square.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //this function is where we will design our interaction using code 
+    protected override void Interact()
+    {
+        Debug.Log("Interact with" + gameObject.name);
+    }
+
+        
+}
--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class PlayerInteract : MonoBehaviour
+public class PLayerUi : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI promptText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,8 @@ public class PlayerInteract : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpdateText(string promptMessage)
     {
-        
+        promptText.text= promptMessage;
     }
 }
