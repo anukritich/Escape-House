cf3d2e914a10c266052ea5941d7897ab5d09cab7
anukritich <chyanukriti@gmail.com>
2024-04-23 09:25:49 +0530

    Colour Recognition

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,16 +1,16 @@
-//using UnityEngine;
+using UnityEngine;
 
-//public class LampGlow : MonoBehaviour
-//{
-//    public Light lampLight;
-//    public float glowSpeed = 1.0f;
-//    public float minIntensity = 0.0f;
-//    public float maxIntensity = 2.0f;
+public class LampGlow : MonoBehaviour
+{
+    public Light lampLight;
+    public float glowSpeed = 1.0f;
+    public float minIntensity = 0.0f;
+    public float maxIntensity = 2.0f;
 
-//    private void Update()
-//    {
-//        // Calculate the intensity based on time
-//        float intensity = Mathf.Lerp(maxIntensity, minIntensity, Time.time * glowSpeed);
-//        lampLight.intensity = intensity;
-//    }
-//}
+    private void Update()
+    {
+        // Calculate the intensity based on time
+        float intensity = Mathf.Lerp(maxIntensity, minIntensity, Time.time * glowSpeed);
+        lampLight.intensity = intensity;
+    }
+}
